720928abfd18b266a6a83bbfac21656dc0c103c6
german-ipesriera <german.ipesriera@ukg.com>
2024-03-11 17:52:00 -0300

    first commit

---
+++ src/main/java/com/example/KeepCodingAndNobodyExplodes/KeepCodingAndNobodyExplodesApplication.java
@@ -0,0 +1,13 @@
+package com.example.KeepCodingAndNobodyExplodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepCodingAndNobodyExplodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepCodingAndNobodyExplodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KeepCodingAndNobodyExplodes/KeepCodingAndNobodyExplodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KeepCodingAndNobodyExplodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepCodingAndNobodyExplodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
